c31c5b141bf3ed444ce1501c2aeb68a2fb144243
Umberto Amenta <umberto.amenta@hotmail.it>
2024-12-05 22:35:11 +0100

    feat: +01-iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,9 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const getfirstCharacters = (list) => {
+    let firstCharacterList = []
+    for (let i = 0; i < list.length; i++) {
+        let firstCharacter = list[i].charAt(0)
+        firstCharacterList.push(firstCharacter)
+    }
+    return firstCharacterList
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+getfirstCharacters(names)
+console.log(getfirstCharacters(names))
 
 
 
