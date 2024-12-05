a5baf1025b7217db6a1c24641cc7e6871f48a7be
Umberto Amenta <umberto.amenta@hotmail.it>
2024-12-05 17:35:47 +0100

    feat: +04-saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,9 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+const getHello = (userName) => `Ciao ${userName}`
 
 
 // Invoca la funzione qui e stampa il risultato in console
+getHello(userName)
+console.log(getHello(userName))
 
 
 
