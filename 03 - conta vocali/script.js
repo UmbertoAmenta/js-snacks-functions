a9dbea67b0b59a7a3c1c65c5209452d31380179d
Umberto Amenta <umberto.amenta@hotmail.it>
2024-12-08 13:15:46 +0100

    feat: add alternative

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -9,12 +9,24 @@ const word = 'javascript';
  * 
  * @param {string} text 
  * @returns 
- */
+*/
 const howManyVowels = (text) => {
-    let textVowels = text.match(/a|e|i|o|u/gi)
-    return textVowels.length
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    let textVowels = "0"
+    for (let i = 0; i < text.length; i++) {
+        const currentChar = text[i]
+        if (vowels.includes(currentChar)) {
+            textVowels++
+        }
+    }
+    return textVowels
 }
 
+// alternativa
+// const howManyVowels = (text) => {
+//     let textVowels = text.match(/a|e|i|o|u/gi)
+//     return textVowels.length
+// }
 
 // Invoca la funzione qui e stampa il risultato in console
 howManyVowels(word)
